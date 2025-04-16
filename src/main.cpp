b419d3a07eaafccaf1da25292a0d955f626b172d
deep <166912166+de-ep@users.noreply.github.com>
2025-04-16 10:26:11 -0400

    daemonize and graceful shutdown

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,45 @@
-#include <cstdio>
 #include <iostream>
 #include <string>
 #include <sys/inotify.h>
 #include <pthread.h>
 #include <queue>
 #include <unistd.h> 
+#include <signal.h>
 #include <filesystem> 
 #include <gpgme.h>
+#include <fcntl.h>
 
 #define MAX_BUFFER_SIZE 1024
 #define MAX_THREADS 8
+#define READ_AGAIN_TIMEOUT 5
 
 
+bool running = true;
 const char* recipient;
 std::string LOG_FILE = "bread.log";
 std::queue<std::string> job;
 pthread_mutex_t job_lock;
 pthread_cond_t job_cond;
 
+
+void signal_handler(int signum) {
+    printf("Signal: %d recieved, exiting\n", signum);
+    running = false;
+    pthread_cond_broadcast(&job_cond);
+}
+
+
+bool set_fd_nonblock(int fd) {
+
+    int flags = fcntl(fd, F_GETFL);
+    if (flags == -1)    
+        return 0;
+
+    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1)
+        return 0;
+
+    return 1;
+}
 
 int encrypt(const char* file) {
 
@@ -30,12 +52,6 @@ int encrypt(const char* file) {
     size_t size;
     FILE* fd; 
 
-
-    const char* version = gpgme_check_version("1.24.0");
-    if (version == NULL) {
-        fprintf(stderr, "GPGME version check failed\n");
-        goto cleanup;
-    }
 
     err = gpgme_new(&ctx);
     if (err != GPG_ERR_NO_ERROR) {
@@ -123,12 +139,19 @@ void* handle_thread (void* tid){
         pthread_mutex_lock(&job_lock); 
         pthread_cond_wait(&job_cond, &job_lock);
 
+        //if we get here and there are no jobs or !running, the thread was woken up by signal handler and we need to exit 
+        if (job.empty() || !running) {
+            pthread_mutex_unlock(&job_lock);
+            break;
+        }
+
         path = job.front();
         job.pop();
         
         pthread_mutex_unlock(&job_lock);
 
         
+        //if the job is a dir, push the contents of dir in job queue 
         if (std::filesystem::is_directory(path)) {
             pthread_mutex_lock(&job_lock); 
 
@@ -150,7 +173,7 @@ void* handle_thread (void* tid){
             printf("Failed to encrypt: %s\n", path.c_str());
 
     }
-
+    return 0;
 }
 
 
@@ -159,7 +182,18 @@ int main (int argc, char* argv[]) {
         fprintf(stderr, "Usage: ./bread <directory> <recipient>\n");
         return EXIT_FAILURE;
     }
-    
+
+    const char* version = gpgme_check_version("1.24.0");
+    if (version == NULL) {
+        fprintf(stderr, "GPGME version check failed\nExpected version: 1.24.0\n");
+        return EXIT_FAILURE;
+    }
+
+    if (daemon(1, 0) == -1) {
+        perror("Unable to daemonize the process");
+        return EXIT_FAILURE;
+    }
+
     std::string dir_path = argv[1];
     if (dir_path.back() != '/')
         dir_path.append("/");
@@ -179,7 +213,11 @@ int main (int argc, char* argv[]) {
     int iaw = inotify_add_watch(fd, dir_path.c_str(), IN_MOVED_TO);
     if (iaw == -1)
         exit(1);
-
+    
+    if (!set_fd_nonblock(fd)){
+        fprintf(stderr, "failed to mark fd as nonblocking");
+        return EXIT_FAILURE;
+    }
     
     pthread_t tid[MAX_THREADS];
     pthread_mutex_init(&job_lock, NULL);
@@ -190,13 +228,23 @@ int main (int argc, char* argv[]) {
     }
 
 
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+
+
     char buf[MAX_BUFFER_SIZE];
-    for (;;) {
-
+    while (running) {
+        
         ssize_t read_count = read(fd, buf, sizeof(buf));
-        if (read_count == -1)
-            exit(1);
-
+        if (read_count == -1) {
+            if (errno == EAGAIN) {  
+                sleep(READ_AGAIN_TIMEOUT);
+                continue;
+            }
+            else
+            return EXIT_FAILURE;
+        }
+        
         size_t prev_len = 0;
         for (;;) {
             
@@ -220,6 +268,15 @@ int main (int argc, char* argv[]) {
         }
     
     }
+
+    cleanup: 
+        if (inotify_rm_watch(fd, iaw) == -1)
+            perror("Failed to remove an existing watch from an inotify instance");
+
+        for (int i = 0 ; i < MAX_THREADS ; i++) {
+            if (pthread_join(tid[i], NULL) != 0)
+                perror("Failed to join thread");
+        }
 
 
     return EXIT_SUCCESS;
